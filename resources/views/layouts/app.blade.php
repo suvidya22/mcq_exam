c804f19595cfd7cf338830be2d71998b86e557a4
suvidya raj <suvidya@iocod.com>
2022-07-31 22:24:13 +0530

    Results

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,8 +7,6 @@
 
 <body class="page-header-fixed">
 
-    @include('partials.analytics')
-
     <div class="page-header navbar navbar-fixed-top">
         @include('partials.header')
     </div>
@@ -23,29 +21,14 @@
         <div class="page-content-wrapper">
             <div class="page-content">
 
-                @if(isset($siteTitle))
+                @if (isset($siteTitle))
                     <h3 class="page-title">
-                        {{ $siteTitle }}
+
                     </h3>
                 @endif
 
                 <div class="row">
                     <div class="col-md-12">
-
-                        @if (Session::has('message'))
-                            <div class="note note-info">
-                                <p>{{ Session::get('message') }}</p>
-                            </div>
-                        @endif
-                        @if ($errors->count() > 0)
-                            <div class="note note-danger">
-                                <ul class="list-unstyled">
-                                    @foreach($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                        @endif
 
                         @yield('content')
 
@@ -55,15 +38,11 @@
         </div>
     </div>
 
-    <div class="scroll-to-top"
-         style="display: none;">
+    <div class="scroll-to-top" style="display: none;">
         <i class="fa fa-arrow-up"></i>
     </div>
 
-    {!! Form::open() !!}
-        <button type="submit">Logout</button>
-    {!! Form::close() !!}
-
     @include('partials.javascripts')
 </body>
+
 </html>
